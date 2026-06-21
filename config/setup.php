1d144112eb92629f57a33cb70ade75d51ad299fb
Nicolas Saavedra <saavedranicolas196@gmail.com>
2026-06-21 18:31:34 -0400

    Corregir conexión en setup.php

--- config/setup.php
+++ config/setup.php
@@ -1,15 +1,16 @@
 <?php
 function conectar() {
-    $servername = $_ENV['DB_HOST'] ?? "localhost";
-    $username   = $_ENV['DB_USER'] ?? "pnk_user";
-    $password   = $_ENV['DB_PASSWORD'] ?? "pass_segura";
-    $dbname     = $_ENV['DB_NAME'] ?? "pnks";
+    $servername = "localhost";
+    $username   = "root";     
+    $password   = "";         
+    $dbname     = "pnks";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
-        die("Conexión fallida: " . $conn->connect_error);
+        die("Error de conexión: " . $conn->connect_error);
     }
     $conn->set_charset("utf8mb4");
     return $conn;
 }
+?>
